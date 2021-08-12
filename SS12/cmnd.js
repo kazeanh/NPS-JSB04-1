1f69af60cc0984d0280ca22fc404d18e1552efb2
hieu1966hn <hieu1966hn@gmail.com>
2021-08-12 20:37:50 +0700

    done function

---
+++ SS12/cmnd.js
@@ -0,0 +1,14 @@
+function changeHandler(events){
+    events.stopPropagation();
+    events.preventDefault();
+    // tránh sự kiện load lại trang
+
+    let files = events.target.files;
+    let file = files[0];
+    let fileReader = new FileReader();
+    fileReader.onload = function(progressEvent){
+        let url = fileReader.result;
+        document.getElementById("image").src = url;
+    }
+    fileReader.readAsDataURL(file);
+}
